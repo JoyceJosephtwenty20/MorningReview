69da5a1cb2aeab2e3d39edc514c512f29dd35e68
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-16 10:36:50 -0400

    first_commit

---
+++ src/MorningReview.java
@@ -0,0 +1,28 @@
+public class MorningReview {
+    public static void  main(String[] args){
+        int num1 = 2;
+        int num2 = num1 + 1;
+
+        System.out.println(num2);
+
+    // square area
+        int length = 4;
+        int width = 4;
+        int area = length * width;
+
+        System.out.println("The are of my square is this: " + area);
+
+
+    // Triangle area
+
+        int base = 6;
+        int height = 3;
+        double triArea = (base * height) * (1/2.0) ;
+
+        System.out.println("The are of my triangle is this: " + triArea);
+
+
+    }
+
+
+}
